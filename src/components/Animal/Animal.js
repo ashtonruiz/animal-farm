0523030937222cdef93e81711dbc553d8fd7823c
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-09 14:44:14 -0800

    Okay the animals are rendering but I have this mysterious broken image in the upper left corner and I don't know what it is or why it's there... Going to try and debug that now lol

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './Animal.css';
 
-export default function Animal(props) {
+export default function Animal({ name, says, top, left, type }) {
   return (
-    <div className="animal">
-      <img src="animals/cow.svg" />
-      <h2>{props.name}</h2>
+    <div className="animal" style={{ top, left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
     </div>
   );
 }
